749d9a5c196d860abbebcb7270884c2fcea85ff8
Adriano Caloiaro <code@adriano.fyi>
2025-04-14 10:24:54 -0600

    fix: overlapping cgo enabled (default.nix)

--- default.nix
+++ default.nix
@@ -4,7 +4,6 @@
   ...
 }:
 pkgs.buildGoModule rec {
-  env.CGO_ENABLED = 0;
   pname = "ess";
   version = pkgs.lib.strings.removeSuffix "\n" (builtins.readFile ./version.txt);
   src = ./.;
@@ -13,6 +12,7 @@ pkgs.buildGoModule rec {
     "-X 'main.version=${version}-nix'"
     "-X 'main.commit=${self.rev or "dev"}'"
   ];
+  CGO_ENABLED = 0;
 
   meta = {
     description = "ess (env-sample-sync): automatically and safely synchronize env.sample files with .env";
